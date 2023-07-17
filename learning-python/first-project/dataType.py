c05e6f6755703f9e3247dd3f6f5c4b3d91e7f27d
rohan-mostofa <rohanmostofa599@gmail.com>
2023-07-17 20:37:50 +0600

    updated

--- learning-python/first-project/dataType.py
+++ learning-python/first-project/dataType.py
@@ -1,4 +1,37 @@
+# str data type
+a = "Rohan"
+print(type(a))
 
+# int data type
+b = 999
+print(type(b))
+
+# float data type
+c = 99.9
+print(type(c))
+print(c)
+
+# complex data type
+d = 545j
+print(type(d))
+print(d)
+
+# bool data type
+e = True
+print(type(e))
+f = 13
+g = 7
+print(f > g)
+# super
+print(f"Answare = {f + g}")
+
+# binary data type
+
+
+# combine variable
+x = "Rohan"
+y = "Mostofa"
+print("My name is " + x + " " + y)
 
 
 
