791088cf523d6fb27b0f670ebe2ffc3f366574ec
rohan-mostofa <rohanmostofa599@gmail.com>
2023-07-18 20:10:45 +0600

    updated

--- learning-vuejs/vue-project/src/main.js
+++ learning-vuejs/vue-project/src/main.js
@@ -5,6 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import router from "./router/index.js";
 
 const app = createApp(App);
+app.component(
+    // the registered name
+    'MyComponent',
+    // the implementation
+    {
+        /* ... */
+    }
+)
 app.use(router);
 app.mount('#app');
 
