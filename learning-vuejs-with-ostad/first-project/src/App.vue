51afa6b3d6a8e1c805b59871ead24dcb01a7e1bd
dev-rohan99 <rohanmostofa599@gmail.com>
2024-07-29 19:58:33 +0600

    learning vuejs with ostad

---
+++ learning-vuejs-with-ostad/first-project/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+  const message = "Hello World!";
+  const date = new Date()
+  date.toString();
+
+</script>
+
+<template>
+
+  <div>
+    <h1>{{message}}</h1>
+    <h4>{{date}}</h4>
+  </div>
+
+</template>
+
+<style scoped>
+
+
+
+
+</style>
